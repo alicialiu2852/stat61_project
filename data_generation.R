e8991e182de5ce8f84400de56286f4a2b4d44fbc
alicialiu2852 <90984613+alicialiu2852@users.noreply.github.com>
2023-12-19 18:43:38 -0500

    Upload generated data and code to generate data.

---
+++ data_generation.R
@@ -0,0 +1,18 @@
+# Generate mystery data
+simulated_data3 = tibble(
+x = rt(100, 6)
+)
+write.csv(simulated_data3, "mystery_data.csv", row.names = FALSE)
+
+#Generate distribution test data
+distTest_data1 <- tibble(
+  x = rnorm(10000)
+)
+
+add_data <- tibble(
+  x = c(-4)
+)
+
+distTest_data1 <- bind_rows(distTest_data1, add_data)
+
+write.csv(distTest_data1, "distTest_data.csv", row.names = FALSE)
